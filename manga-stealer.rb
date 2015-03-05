d36319c927ac248f680d805674eb01ccce3bc6fc
Eve <eveleech@gmail.com>
2015-03-05 04:53:29 +0400

    removed Zip/zip

--- manga-stealer.rb
+++ manga-stealer.rb
@@ -1,11 +1,9 @@
 #!/usr/bin/ruby
-require 'zip'
 require 'mechanize'
 url = "http://mangachan.ru/download/37563-she-is-young.html"
 dest = "./data/"
 links = Mechanize.new.get(url).links.select { |link| link.text.end_with? (".zip")}.reverse!
-links.each do |link|
+links.each_with_index do |link,chapter|
   link.click.save("#{dest}#{link.text}")
-  `unzip #{dest}#{link.text} -d #{dest}#{link.text.gsub(".zip","")}`
-  File.delete("#{dest}#{link.text}") if File.exist?("#{dest}#{link.text}")
+  `unzip #{dest}#{link.text} -d #{dest}#{chapter+1} && rm #{dest}#{link.text}`
 end
